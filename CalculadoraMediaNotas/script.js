d067513bb0ce62d102aa29d830e7ce0c32b2c297
Yuri Luiz <141971887+yuuhLKT@users.noreply.github.com>
2024-01-08 20:16:01 -0300

    fixed JavaScript

--- CalculadoraMediaNotas/script.js
+++ CalculadoraMediaNotas/script.js
@@ -7,55 +7,54 @@ const corpoTable = document.getElementById("corpo-table");
 const mediaFinalInput = document.getElementById("media-final");
 const mediaAtv = document.getElementById("media-atv");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   adicionarAtividadeTable();
   calcularMedia();
-  adicionarMediaTable()
+  adicionarMediaTable();
 });
 
-function adicionarAtividadeTable(){
+function adicionarAtividadeTable() {
   let linhaBodyTable = '<tr>';
   linhaBodyTable += `<td>${nomeAtv.value}</td>`;
   linhaBodyTable += `<td>${parseFloat(notaAtv.value).toFixed(2)}</td>`;
   linhaBodyTable += `<td>${parseFloat(mediaAtv.value).toFixed(2)}</td>`;
   linhaBodyTable += `<td>${parseFloat(notaAtv.value) >= parseFloat(mediaAtv.value) ? imgAprovado : imgReprovado}</td>`;
   linhaBodyTable += '</tr>';
-  
+
   corpoTable.innerHTML += linhaBodyTable;
-  
+
   nomeAtv.value = "";
   notaAtv.value = "";
   mediaAtv.value = "";
 }
 
-function calcularMedia(){
-  
+function calcularMedia() {
   let somaNotas = 0;
-  
+
   for (let i = 0; i < corpoTable.rows.length; i++) {
     somaNotas += parseFloat(corpoTable.rows[i].cells[1].innerText);
   }
-  
+
   return parseFloat(somaNotas / corpoTable.rows.length).toFixed(2);
 }
 
 function adicionarMediaTable() {
   const media = calcularMedia();
-  const mediaFinal = parseFloat(mediaFinalInput.value);
+  let mediaFinal = parseFloat(mediaFinalInput.value);
+
+  if (mediaFinal < 0) {
+    mediaFinal = 0;
+  } else if (mediaFinal > 10) {
+    mediaFinal = 10;
+  }
+
   const aprovadoTxt = '<span class="resultado aprovado">Aprovado</span>';
   const reprovadoTxt = '<span class="resultado reprovado">Reprovado</span>';
 
-  let linhaMediaFinal = '<tr>';
-  linhaMediaFinal += `<td><strong>Media Final</strong></td>`;
-  linhaMediaFinal += `<td><strong>${media}</strong></td>`;
-  linhaMediaFinal += `<td><strong>${mediaFinal.toFixed(2)}</strong></td>`;
-  linhaMediaFinal += `<td>${media >= mediaFinal ? aprovadoTxt : reprovadoTxt}</td>`;
-  linhaMediaFinal += '</tr>';
-
-  const footerTable = document.getElementById("footer-table");
-
-  footerTable.innerHTML = linhaMediaFinal;
+  document.getElementById("resultado-media").innerHTML = `${media}`;
+  document.getElementById("media-necessaria").innerHTML = `${mediaFinal.toFixed(2)}`;
+  document.getElementById("situacao-media").innerHTML = media >= mediaFinal ? aprovadoTxt : reprovadoTxt;
 }
 
 mediaFinalInput.addEventListener("change", adicionarMediaTable);
